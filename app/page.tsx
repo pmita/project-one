c135f957127f795ee1a9294e791b3df6e3d770d4
pmita <panos.mita2@outlook.com>
2024-07-21 21:23:57 +0100

    style: remove unneccesary gap from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <h1>Welcome to the start</h1>
     </main>
   );
