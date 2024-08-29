7f4e7c36b4880c7f2cb35d5f8b215ef150611959
pmita <panos.mita2@outlook.com>
2024-08-29 11:46:08 +0100

    chore: installing firebase-admin

---
+++ firebase/server/config.ts
@@ -0,0 +1,22 @@
+import admin from 'firebase-admin'
+
+if(!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      client_email: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_CLIENT_EMAIL,
+      private_key: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PRIVATE_KEY,
+      project_id: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PROJECT_ID
+    } as admin.ServiceAccount),
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  });
+}
+
+// SERVICES
+export const firestore = admin.firestore();
+
+// HELPERS
+export const serverTimestamp = admin.firestore.FieldValue.serverTimestamp;
+export const increment = admin.firestore.FieldValue.increment;
+export const fromMillis = admin.firestore.Timestamp.fromMillis;
+export const arrayUnion = admin.firestore.FieldValue.arrayUnion;
+export const arrayRemove = admin.firestore.FieldValue.arrayRemove;
