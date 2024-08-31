53704cdc3222b44c8372725580209fdc4f9a2ab2
pmita <panos.mita2@outlook.com>
2024-08-31 20:41:45 +0100

    feat(dynamic-routing): display item info

--- app/dashboard/[id]/page.tsx
+++ app/dashboard/[id]/page.tsx
@@ -1,5 +1,7 @@
 // DATA
 import { getDocumentData } from "@/data/db";
+import { ItemInfo } from "./_components/ItemInfo";
+import { IQueryItem } from "@/types/db";
 
 interface DashboardItemPageProps {
   params: {
@@ -17,14 +19,14 @@ export default async function ItemPage({ params }: DashboardItemPageProps) {
   return (
     <section className="flex flex-col justify-center items-stretch gap-4 p-8">
       <div className="grid grid-cols-1 grid-rows-[250px] gap-4 lg:grid-cols-2 lg:gap-8">
-        <div className="rounded-lg bg-neutral flex flex-col justify-center items-stretch gap-4 p-2 lg:p-4">
-          <h1>Item status will go here</h1>
+        <div className="rounded-lg bg-neutral gap-4 p-2 lg:p-4">
+          <ItemInfo item={item as IQueryItem} />
         </div>
         <div className="rounded-lg bg-neutral flex flex-col justify-center items-stretch gap-4 p-2 lg:p-4">
-          <h1>Status update will go here</h1>
+          <h1>Status update will go here - realtime details</h1>
         </div>
         <div className="rounded-lg bg-neutral lg:col-span-2 p-4 flex flex-col gap-4">
-          <h1>Comments will go here</h1>
+          <h1>Comments will go here - realtime details</h1>
         </div>
       </div>
     </section>
