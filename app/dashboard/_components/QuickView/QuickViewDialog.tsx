629fae657fca7dfa50168c3d9694aa14dbd22561
pmita <panos.mita2@outlook.com>
2024-09-01 23:09:45 +0100

    feat(quick-view): combining dialogs with quick view feature

---
+++ app/dashboard/_components/QuickView/QuickViewDialog.tsx
@@ -0,0 +1,65 @@
+"use client"
+
+import { useRef, useEffect } from "react";
+// COMPONENTS
+import { Button, buttonVariants } from "@/components/Button";
+import { Comments } from "@/components/Comments";
+import { ItemInfo } from "../../[id]/_components/ItemInfo";
+import { Dialog, DialogHeader, DialogPanel, DialogTitle, DialogDescription } from "@/components/Dialog";
+// HOOKS
+import { useCollectionSnapshot } from "@/hooks/useCollectionSnapshot";
+// UTILS
+import { cn } from "@/utils/helpers";
+// TYPES
+import { IQueryItem } from "@/types/db";
+// STYLES
+import styles from './styles.module.css';
+
+type QuickViewDialogProps = {
+    item: IQueryItem
+    isOpen: boolean
+    toggleDialog: (option: boolean) => void
+}
+
+export const QuickViewDialog = ({ item, isOpen, toggleDialog }: QuickViewDialogProps) => {
+    // STATE && VARIABLES
+    const dialogRef = useRef<HTMLDialogElement>(null);
+    const { data: realtimeComments } = useCollectionSnapshot(`queries/${item.id}/comments`, { sort: 'asc' });
+
+    // USE EFFECTS
+    useEffect(() => {
+        if(isOpen) {
+            dialogRef.current?.showModal()
+        } else {
+            dialogRef.current?.close()
+        }
+    }, [isOpen,dialogRef])
+
+    return (
+        <Dialog ref={dialogRef}>
+            <DialogPanel>
+                <DialogHeader className={`${styles.header}`}>
+                    <DialogTitle>
+                        <Button 
+                            className={cn(buttonVariants({ variant: "primary" }))}
+                            onClick={() => toggleDialog(false)}
+                        >
+                            Close
+                        </Button>
+                    </DialogTitle>
+                    <ItemInfo item={item as IQueryItem} />
+                </DialogHeader>
+                <DialogDescription>
+                    <div className={`${styles.commentsContainer}`}>
+                        <Comments
+                            id={item.id}
+                            status={item.status}
+                            comments={realtimeComments}
+                            canAddComments={true}
+                        />
+                    </div>
+                </DialogDescription>
+            </DialogPanel>
+        </Dialog>
+    )
+}
