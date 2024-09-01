dfca9185358121a238b0fed39e25b40ef6fbf53b
pmita <panos.mita2@outlook.com>
2024-09-01 22:42:01 +0100

    feat(dialogs): adding generic dialog component

---
+++ components/Dialog/Dialog.tsx
@@ -0,0 +1,81 @@
+// REACT
+import React from "react"
+// UTILS
+import { cn } from "@/utils/helpers"
+// TYPES
+import { IDialog, IDialogPanel } from "./types"
+// STYLES
+import styles from "./styles.module.css"
+
+export const Dialog = React.forwardRef<HTMLDialogElement, IDialog>(({ className, children, ...props }, ref) => {
+    return (
+      <dialog className={cn(
+       `${styles.container}`,
+        className
+      )}
+      {...props}
+      ref={ref}
+      >
+        {children}
+      </dialog>
+    )
+  })
+  Dialog.displayName = "Dialog";
+
+  export const DialogPanel = ({ className, children, ...props }: IDialogPanel) => {
+    return (
+      <div className={cn(
+        `${styles.panel}`,
+        className
+      )}
+      {...props}
+      >
+        {children}
+      </div>
+    )
+  }
+
+  export const DialogHeader = ({ className, ...props }: IDialogPanel) => {
+    return (
+      <div className={cn(
+       `${styles.header}`,
+        className
+        )}
+        {...props}
+      />
+    )
+  }
+  
+  export const DialogTitle = React.forwardRef<HTMLDivElement, IDialogPanel>(({ className, ...props }) => {
+    return (
+      <div className={cn(
+       `${styles.title}`,
+        className
+        )}
+        {...props}
+      />
+    )
+  })
+  DialogTitle.displayName = "DialogTitle";
+  
+  export const DialogDescription = ({ className, ...props }: IDialogPanel) => {
+    return (
+      <div className={cn(
+       `${styles.description}`,
+        className
+        )}
+        {...props}
+      />
+    )
+  }
+  
+  export const DialogFooter = ({ className, ...props }: IDialogPanel) => {
+    return (
+      <div className={cn(
+       `${styles.footer}`,
+        className
+        )}
+        {...props}
+      />
+    )
+  }
