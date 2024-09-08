93e3747d2ee37ba56a88293a22d6d8b03cd7968c
pmita <panos.mita2@outlook.com>
2024-09-08 19:29:30 +0100

    style: fixing ItemCard styling issues

--- app/dashboard/_components/ItemCard/ItemCard.tsx
+++ app/dashboard/_components/ItemCard/ItemCard.tsx
@@ -11,22 +11,22 @@ import { cn, truncate } from '@/utils/helpers';
 // TYPES
 import { IQueryItem } from '@/types/db';
 // STYLES
-// import styles from './styles.module.css';
+import styles from './styles.module.css';
 
 export const ItemCard = ({ item }: { item : IQueryItem | null }) => {
 
   if (!item) return null;
 
   return (
-    <Card className="flex flex-col justify-center items-stretch">
-      <CardHeader className="flex-1 flex-row justify-between items-center">
+    <Card className={`${styles.itemContainer}`}>
+      <CardHeader className={`${styles.itemHeader}`}>
         <Status status={item.status} />
         <FormatedTime time={item.createdAt} />
       </CardHeader>
-      <CardDescription className="flex-1">
+      <CardDescription className={`${styles.itemDescription}`}>
         {truncate(item.additionalInfo, 150)}
       </CardDescription>
-      <CardFooter className="flex-1 flex-col justify-center items-stretch gap-2">
+      <CardFooter className={`${styles.itemFooter}`}>
         <Link 
             className={cn(buttonVariants({ variant: 'primary', size: 'sm' }))}
             href={`/dashboard/${item.id}`}
